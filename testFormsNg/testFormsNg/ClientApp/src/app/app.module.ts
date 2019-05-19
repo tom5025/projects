c2d45481499da0b226b8e2941a4b290309c08bc9
tom5025 <thomas.remy71@gmail.com>
2019-05-19 15:39:24 +0200

    prg

--- testFormsNg/testFormsNg/ClientApp/src/app/app.module.ts
+++ testFormsNg/testFormsNg/ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, FormBuilder } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -9,7 +9,6 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { Http } from '@angular/http';
 
 @NgModule({
   declarations: [
@@ -21,16 +20,16 @@ import { Http } from '@angular/http';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    ReactiveFormsModule,
     HttpClientModule,
-    FormsModule,
-    FormBuilder,
+    FormsModule,        
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
-  providers: [FormBuilder, Http],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- testFormsNg/testFormsNg/ClientApp/src/app/home/home.component.ts
+++ testFormsNg/testFormsNg/ClientApp/src/app/home/home.component.ts
@@ -8,54 +8,66 @@ import 'rxjs/Rx';
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  personForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    
+  }
 
-    personForm: FormGroup;
-    successfulSave: boolean;
-    errors: string[];
- 
-    constructor(private fb: FormBuilder, private http: Http) {
-    }
- 
-    ngOnInit() {
-        this.personForm = this.fb.group({
-            title: [''],
-            firstName: [''],
-            surname: [''],
-            emailAddress: ['']
-        });
-        this.errors = [];
-    }
- 
-    onSubmit() {
-        if (this.personForm.valid) {
-            let headers = new Headers({ 'Content-Type': 'application/json' });
-            let options = new RequestOptions({ headers: headers });
-            let person = {
-                title: this.personForm.value.title,
-                firstName: this.personForm.value.firstName,
-                surname: this.personForm.value.surname,
-                emailAddress: this.personForm.value.emailAddress
-            };
-            this.errors = [];
-            this.http.post('/api/person', JSON.stringify(person), options)
-                .map(res => res.json())
-                .subscribe(
-                    (data) => this.successfulSave = true,
-                    (err) => {
-                        this.successfulSave = false;
-                        if (err.status === 400) {
-                            // handle validation error
-                            let validationErrorDictionary = JSON.parse(err.text());
-                            for (var fieldName in validationErrorDictionary) {
-                                if (validationErrorDictionary.hasOwnProperty(fieldName)) {
-                                    this.errors.push(validationErrorDictionary[fieldName]);
-                                }
-                            }
-                        } else {
-                            this.errors.push("something went wrong!");
-                        }
-                    });
-        }
-    }
+  ngOnInit() {
+    this.personForm = this.fb.group({
+      name: ['']
+    });
+  }
+  //name = new FormControl('');
 }
+  //implements OnInit {
+
+    //personForm: FormGroup;
+    //successfulSave: boolean;
+    //errors: string[];
+ 
+    //constructor(private fb: FormBuilder, private http: Http) {
+    //}
+ 
+    //ngOnInit() {
+    //    this.personForm = this.fb.group({
+    //        title: [''],
+    //        firstName: [''],
+    //        surname: [''],
+    //        emailAddress: ['']
+    //    });
+    //    this.errors = [];
+    //}
+ 
+    //onSubmit() {
+    //    if (this.personForm.valid) {
+    //        let headers = new Headers({ 'Content-Type': 'application/json' });
+    //        let options = new RequestOptions({ headers: headers });
+    //        let person = {
+    //            title: this.personForm.value.title,
+    //            firstName: this.personForm.value.firstName,
+    //            surname: this.personForm.value.surname,
+    //            emailAddress: this.personForm.value.emailAddress
+    //        };
+    //        this.errors = [];
+    //        this.http.post('/api/person', JSON.stringify(person), options)
+    //            .map(res => res.json())
+    //            .subscribe(
+    //                (data) => this.successfulSave = true,
+    //                (err) => {
+    //                    this.successfulSave = false;
+    //                    if (err.status === 400) {
+    //                        // handle validation error
+    //                        let validationErrorDictionary = JSON.parse(err.text());
+    //                        for (var fieldName in validationErrorDictionary) {
+    //                            if (validationErrorDictionary.hasOwnProperty(fieldName)) {
+    //                                this.errors.push(validationErrorDictionary[fieldName]);
+    //                            }
+    //                        }
+    //                    } else {
+    //                        this.errors.push("something went wrong!");
+    //                    }
+    //                });
+    //    }
+    //}
