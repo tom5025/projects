c68787db190b849c9e8f4c1c137f3df9c701dbb0
tom5025 <thomas.remy71@gmail.com>
2019-05-30 11:34:10 +0200

    Test shopify plugin development with React/Next and Node.js

---
+++ testshopify/server.js
@@ -0,0 +1,110 @@
+require('isomorphic-fetch');
+const Koa = require('koa');
+const next = require('next');
+const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
+const dotenv = require('dotenv');
+const { verifyRequest } = require('@shopify/koa-shopify-auth');
+const session = require('koa-session');
+
+dotenv.config();
+const { default: graphQLProxy } = require('@shopify/koa-shopify-graphql-proxy');
+const { ApiVersion } = require('@shopify/koa-shopify-graphql-proxy');
+const Router = require('koa-router');
+const { receiveWebhook, registerWebhook } = require('@shopify/koa-shopify-webhooks');
+const processPayment = require('./server/router');
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const {
+  SHOPIFY_API_SECRET_KEY,
+  SHOPIFY_API_KEY,
+  TUNNEL_URL,
+  API_VERSION
+} = process.env;
+
+app.prepare().then(() => {
+  const server = new Koa();
+  const router = new Router();
+  server.use(session(server));
+  server.keys = [SHOPIFY_API_SECRET_KEY];
+
+  router.get('/', processPayment);
+
+  server.use(
+    createShopifyAuth({
+      apiKey: SHOPIFY_API_KEY,
+      secret: SHOPIFY_API_SECRET_KEY,
+      scopes: ['read_products', 'write_products'],
+      async afterAuth(ctx) {
+        const { shop, accessToken } = ctx.session;
+        ctx.cookies.set('shopOrigin', shop, { httpOnly: false });
+
+        const stringifiedBillingParams = JSON.stringify({
+          recurring_application_charge: {
+            name: 'Recurring charge',
+            price: 20.01,
+            return_url: TUNNEL_URL,
+            test: true,
+          },
+        });
+
+        const registration = await registerWebhook({
+          address: `${TUNNEL_URL}/webhooks/draft_orders/create`,
+          topic: 'DRAFT_ORDERS_CREATE',
+          accessToken,
+          shop,
+        });
+
+        if (registration.success) {
+          console.log('Successfully registered webhook!');
+        } else {
+          console.log('Failed to register webhook', registration.result);
+        }
+        
+        const options = {
+          method: 'POST',
+          body: stringifiedBillingParams,
+          credentials: 'include',
+          headers: {
+            'X-Shopify-Access-Token': accessToken,
+            'Content-Type': 'application/json'
+          }
+        };
+
+        const confirmationURL = await fetch(
+          `https://${shop}/admin/api/${API_VERSION}/recurring_application_charges.json`, options,
+)
+          .then((response) => response.json())
+          .then((jsonData) => jsonData.recurring_application_charge.confirmation_url)
+          .catch((error) => console.log('error', error));
+        ctx.redirect(confirmationURL);
+      }
+    })
+  );
+
+  const webhook = receiveWebhook({ secret: SHOPIFY_API_SECRET_KEY });
+
+
+  router.post('webhooks/draft_orders/create', webhook, (ctx) => {
+    console.log('received order creation');
+  });
+
+
+  server.use(graphQLProxy({ version: ApiVersion.April19 }));
+
+  router.get('*', verifyRequest(), async (ctx) => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+    ctx.res.statusCode = 200;
+  });
+
+  server.use(router.allowedMethods());
+  server.use(router.routes());
+
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
