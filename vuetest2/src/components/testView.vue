eed30a46d228ede2a9b87fe04432c1efe54723e6
tom5025 <thomas.remy71@gmail.com>
2019-06-12 12:29:53 +0200

    vue test

--- vuetest2/src/components/testView.vue
+++ vuetest2/src/components/testView.vue
@@ -1,8 +1,10 @@
+<template>
 <div class="row">      
     <div class="col-md-4 col-lg4" v-for="(data,index) in products" :key="index">        
         <h3>{{data.productTitle}}</h3>      
     </div>    
 </div>
+</template>
 <script>
 export default {
   name: "home",
