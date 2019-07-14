72fed2141b41f2c514f023652abadd75b6fd24c8
tom5025 <thomas.remy71@gmail.com>
2019-07-14 21:13:47 +0200

    progress

--- cartutorialvue/src/components/BrandList.vue
+++ cartutorialvue/src/components/BrandList.vue
@@ -1,11 +1,31 @@
 <template>
-  <v-container>
-    <v-layout>
-      <v-flex xs1 v-for="brand in brandList" :key="brand">
-        {{ brand }}
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <!-- <v-card-actions>
+          <v-select v-model="size" :items="items" label="Size"></v-select>
+          <v-spacer></v-spacer>
+        </v-card-actions> -->
+        <v-container v-bind="{ [`grid-list-${size}`]: true }" fluid>
+          <v-layout row wrap>
+            <v-flex
+              v-for="brand in brandList"
+              :key="brand"
+              xs4
+            >
+              <v-card tile>
+                {{brand}}
+                <!-- <v-img
+                  :src="`https://unsplash.it/150/300?image=${Math.floor(Math.random() * 100) + 1}`"
+                  height="150px"
+                ></v-img> -->
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
